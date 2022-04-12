24a6af3e3bac002fe9ba9bdb295a3e8a0679e321
Cappy Ishihara <cappy@cappuchino.xyz>
2022-04-13 02:25:27 +0700

    transmission config

--- nix/services.nix
+++ nix/services.nix
@@ -10,5 +10,10 @@
     enable = true;
     openFirewall = true;
     home = "/srv/nas/config/transmission";
+    settings = {
+        rpc-whitelist-enabled = false;
+        rpc-bind-address = "0.0.0.0";
+        download-dir = "/srv/nas/storage/Torrents/Transmission"
+    };
   };
 }
