8fe9553bf67510ace370d64c98cf8016ed0c30d1
Cappy Ishihara <cappy@cappuchino.xyz>
2022-04-12 23:28:26 +0700

    ws

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      "${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/master.tar.gz" }/raspberry-pi/4"
+      "<nixos-hardware/raspberry-pi/4>"
     ];
 
   #  === Bootloader configuration. ===
